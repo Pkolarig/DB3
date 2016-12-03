451ecbe15d539a99267437494fcabb27beaaaaf4
Pkolarig <pkolarig@uwo.ca>
2016-12-03 10:16:22 -0500

    Update init.py

--- init.py
+++ init.py
@@ -149,7 +149,7 @@ def showmoviegenre():
 def del_genre():
     id = request.args.get('id')
     genre = request.args.get('genre')
-   print
+   print (id)
     cnx = mysql.connector.connect(user='root', database='MovieTheatre')
     cursor = cnx.cursor()
     delete_genre = (
